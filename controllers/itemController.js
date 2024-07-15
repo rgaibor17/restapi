18d9a910d7db07c948ff782849ce8022955fc4a9
rgaibor17 <108085022+rgaibor17@users.noreply.github.com>
2024-07-15 13:58:46 -0500

    Guía 16 - Class 16 - 2

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,20 +4,20 @@ const admin = require('firebase-admin');
  exports.createItem = async (req, res) => {
 
   /* 
-     #swagger.tags = ['Items']
-     #swagger.description = 'Create an item'
-     #swagger.summary = 'Create an item'
-     #swagger.parameters['data'] = {
-         in: 'body',
-         description: 'Data to create an item',
-         required: true,
-     }
-     #swagger.responses[201] = {
-         description: 'Item successfully created',
-     }
-     #swagger.responses[400] = {
-         description: 'Bad request',
-     }
+        #swagger.tags = ['Items']
+        #swagger.description = 'Create an item'
+        #swagger.summary = 'Create an item'
+        #swagger.parameters['data'] = {
+            in: 'body',
+            description: 'Data to create an item',
+            required: true,
+        }
+        #swagger.responses[201] = {
+            description: 'Item successfully created',
+        }
+        #swagger.responses[400] = {
+            description: 'Bad request',
+        }
   */
 
   try {
@@ -32,15 +32,15 @@ const admin = require('firebase-admin');
  exports.getAllItems = async (req, res) => {
 
   /* 
-     #swagger.tags = ['Items']
-     #swagger.description = 'Get all items entries'
-     #swagger.summary = 'Get all items entries'
-     #swagger.responses[200] = {
-         description: 'Items entries successfully obtained',
-     }
-     #swagger.responses[400] = {
-         description: 'Bad request',
-     }
+        #swagger.tags = ['Items']
+        #swagger.description = 'Get all items entries'
+        #swagger.summary = 'Get all items entries'
+        #swagger.responses[200] = {
+            description: 'Items entries successfully obtained',
+        }
+        #swagger.responses[400] = {
+            description: 'Bad request',
+        }
   */
 
   try {
@@ -55,24 +55,24 @@ const admin = require('firebase-admin');
 
 exports.getItem = async (req, res) => {
 
-  /* 
-     #swagger.tags = ['Items']
-     #swagger.description = 'Get an item entry'
-     #swagger.summary = 'Get an item entry'
-     #swagger.parameters['id'] = {
-         description: 'Item id',
-         required: true,
-     }
-     #swagger.responses[404] = {
-         description: 'Item not found',
-     }
-     #swagger.responses[400] = {
-         description: 'Bad request',
-     }
-     #swagger.responses[200] = {
-         description: 'Get an item by id',
-     }
-  */
+    /* 
+        #swagger.tags = ['Items']
+        #swagger.description = 'Get an item entry'
+        #swagger.summary = 'Get an item entry'
+        #swagger.parameters['id'] = {
+            description: 'Item id',
+            required: true,
+        }
+        #swagger.responses[404] = {
+            description: 'Item not found',
+        }
+        #swagger.responses[400] = {
+            description: 'Bad request',
+        }
+        #swagger.responses[200] = {
+            description: 'Get an item by id',
+        }
+    */
 
   try {
       const itemId = req.params.id;
@@ -90,6 +90,27 @@ exports.getItem = async (req, res) => {
 
 exports.updateItem = async (req, res) => {
 
+    /* 
+        #swagger.tags = ['Items']
+        #swagger.description = ''
+        #swagger.summary = ''
+        #swagger.parameters['id'] = {
+            description: '',
+            required: true,
+        }
+        #swagger.parameters['data'] = {
+            in: 'body',
+            description: '',
+            required: true,
+        }
+        #swagger.responses[200] = {
+            description: '',
+        }
+        #swagger.responses[400] = {
+            description: '',
+        }
+    */
+
   try {
       const itemId = req.params.id;
       const data = req.body;
@@ -103,6 +124,23 @@ exports.updateItem = async (req, res) => {
 };
 
 exports.deleteItem = async (req, res) => {
+
+    /* 
+    #swagger.tags = ['Items']
+    #swagger.description = ''
+    #swagger.summary = ''
+    #swagger.parameters['id'] = {
+        description: '',
+        required: true,
+    }
+
+    #swagger.responses[200] = {
+        description: '',
+    }
+    #swagger.responses[400] = {
+        description: '',
+    }
+    */
 
   try {
       const itemId = req.params.id;
